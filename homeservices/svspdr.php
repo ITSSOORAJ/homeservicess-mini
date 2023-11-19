95b84a979e95cca04e1b4cb7da52f36c59b8e2f1
SOORAJ KUMAR <121786021+ITSSOORAJ@users.noreply.github.com>
2023-11-19 13:11:15 +0530

    updates

---
+++ homeservices/svspdr.php
@@ -0,0 +1,97 @@
+<?php
+include_once "./include/header.php";
+include_once "./msg/login.php";
+?>
+<!--html>
+    <link rel="stylesheet" href="svpdr.css">
+<div class="canvas-wrapper">
+	<a href="login.php" class="canvas">
+		<div class="canvas_border">
+			<svg>
+				<defs><linearGradient id="grad-orange" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style="stop-color:rgb(253,137,68);stop-opacity:1"></stop><stop offset="100%" style="stop-color:rgb(153,75,23);stop-opacity:1"></stop></linearGradient><linearGradient id="grad-red" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#D34F48"></stop><stop offset="100%" stop-color="#772522"></stop></linearGradient></defs>
+				<rect id="rect-grad" class="rect-gradient" fill="none" stroke="url(#grad-orange)" stroke-linecap="square" stroke-width="4" stroke-miterlimit="30" width="100%" height="100%"></rect>
+			</svg>
+		</div>
+		<div class="canvas_img-wrapper">
+			<img class="canvas_img" src="https://blog.codepen.io/wp-content/uploads/2012/06/Button-Black-Large.png" alt="">
+		</div>
+		<div class="canvas_copy canvas_copy--left">
+			<span class="canvas_copy_subtitle">serviceexpresso</span>
+			<strong class="canvas_copy_title">LOGIN</strong>
+			<strong class="canvas_copy_title">SERVICE PROVIDER</strong>
+			<span class="canvas_copy_details">not registerd?</span>
+		</div>
+	</a>
+	<a href="register.php" class="canvas">
+		<div class="canvas_border">
+			<svg>
+				<defs><linearGradient id="grad-orange" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style="stop-color:rgb(253,137,68);stop-opacity:1"></stop><stop offset="100%" style="stop-color:rgb(153,75,23);stop-opacity:1"></stop></linearGradient><linearGradient id="grad-red" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" stop-color="#D34F48"></stop><stop offset="100%" stop-color="#772522"></stop></linearGradient></defs>
+				<rect id="rect-grad" class="rect-gradient" fill="none" stroke="url(#grad-orange)" stroke-linecap="square" stroke-width="4" stroke-miterlimit="30" width="100%" height="100%"></rect>
+			</svg>
+		</div>
+		<div class="canvas_img-wrapper">
+			<img class="canvas_img" src="https://th.bing.com/th/id/OIP.PamHbXElDqI1D6uUlwLxygHaFO?rs=1&pid=ImgDetMain" alt="">
+		</div>
+		<div class="canvas_copy">
+			<span class="canvas_copy_subtitle">serviceExpresso</span>
+			<strong class="canvas_copy_title">REGISTER</strong>
+			<strong class="canvas_copy_title">AS SERVICE PROVIDER</strong>
+			<span class="canvas_copy_details">Already registered then login</span>
+		</div>
+	</a>
+</div-->
+<html>
+<link rel="stylesheet" href="svpdr.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<!-- about -->
+<div class="about">
+   <a class="bg_links social portfolio" href="https://www.rafaelalucas.com" target="_blank">
+      <span class="icon"></span>
+   </a>
+   <a class="bg_links social dribbble" href="https://dribbble.com/rafaelalucas" target="_blank">
+      <span class="icon"></span>
+   </a>
+   <a class="bg_links social linkedin" href="https://www.linkedin.com/in/rafaelalucas/" target="_blank">
+      <span class="icon"></span>
+   </a>
+   <a class="bg_links logo"></a>
+</div>
+<!-- end about -->
+   
+   <div class="content">
+      <!-- card -->
+      <div class="card">
+         
+            <div class="icon"><i class="material-icons md-36">face</i></div>
+            <a  href="login.php"class="title">LOGIN</a>
+            <p class="text">Click to logon to your account.</p>
+         
+      </div>
+      <!-- end card -->
+      <!-- card -->
+      <div class="card">
+         
+            <div class="icon"><i class="material-icons md-36">favorite_border</i></div>
+            <a href="register.php" class="title">REGISTER</a>
+            <p class="text">Not registered ??then register here as service provider</p>
+         
+      </div>
+      <!-- end card -->
+      <!-- card -->
+      <!--div class="card">
+         
+            <div class="icon"><i class="material-icons md-36">alternate_email</i></div>
+            <p class="title">Contacts</p>
+            <p class="text">Add or change your contacts and links.</p>
+         
+      </div>
+      <!-- end card -->
+   
+
+   
+   </div>
+</div>
+</html>
+
+
---
+++ homeservices/team.php
@@ -0,0 +1,424 @@
+<?php
+include_once "./include/header.php";
+include_once "./msg/login.php";
+?>
+
+<!--html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Team</title>
+    <link rel="icon" type="image/x-icon" href="Images/favicon.ico">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="team.css">
+
+    <style>
+        body {
+            background: url("https://cdn.dribbble.com/users/43762/screenshots/7500309/ubt_dribbbble_04.gif") no-repeat center;
+            
+        }
+    </style>
+</head>
+
+<body-->
+    <!--nav id="mainNavbar" class="navbar navbar-light navbar-expand-md py-1 px-2 fixed-top"
+        style="background-color: #0cb2f9;">
+        <a class="navbar-brand" href="#">
+            <img src="Images/icon.png" width="45" height="35" class="d-inline-block align-middle" alt="">
+            BUS MAESTRO
+        </a>
+
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navLinks" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-between" id="navLinks">
+
+
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="Login/dist/login.php" class="nav-link">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a href="about.html" class="nav-link">ABOUT</a>
+                </li>
+                <li class="nav-item">
+                    <a href="team.html" class="nav-link">TEAM</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Conductor_DashBoard/timetable.php" class="nav-link">TIME TABLE</a>
+                </li>
+
+
+            </ul>
+
+            <span class="nav-item">
+                <a class="nav-link" role="button" href="Login/dist/login.php">Login</a>
+            </span>
+
+        </div>
+    </nav-->
+    <!--div class="container" style="width:50%; height:50%;">
+        <div class="row" style="padding-top: 25%;">
+            <div class="col-sm-6">
+                <div class="our-team">
+                    <img class="img-fluid" src="https://media.licdn.com/dms/image/D5603AQEaZ8EA2nmM8Q/profile-displayphoto-shrink_400_400/0/1693538082331?e=1704326400&v=beta&t=EQGR6tvBy8bcGWuL3N2wsreBRByHCllBLdQuYqB-wEM">
+                    <div class="team-content">
+                        <h3 class="title">SOORAJ KUMAR</h3>
+                        <span class="post">Web Developer</span>
+                        <ul class="social">
+
+                            <li>
+                                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/ITSSOORAJ" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-github"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/sooraj-kumar-s-204a4a267" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-linkedin"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="our-team">
+                    <img class="img-fluid" src="https://media.licdn.com/dms/image/D5603AQFLbkHv6aKeoQ/profile-displayphoto-shrink_800_800/0/1693673710099?e=1704326400&v=beta&t=QRXFkggQT7sVJqI8qi2JHM4uWzTyIpfSrhviHAPEjY4">
+                    <div class="team-content">
+                        <h3 class="title">SHAHUL P.S</h3>
+                        <span class="post">Web Developer</span>
+                        <ul class="social">
+
+                            <li>
+                                <a href="" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/Shahulps" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-github"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="www.linkedin.com/in/muhammadshahulps" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-linkedin"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="our-team">
+                    <img class="img-fluid" src="https://avatars.githubusercontent.com/u/138097979?v=4">
+                    <div class="team-content">
+                        <h3 class="title">NOORJAHAN KA</h3>
+                        <span class="post">Web Developer</span>
+                        <ul class="social">
+
+                            <li>
+                                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/noorjahan-akber" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-github"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/noorjahan-k-a-676161227" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-linkedin"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="our-team">
+                    <img class="img-fluid" src="https://media.licdn.com/dms/image/D5603AQFPK7a7s41tTQ/profile-displayphoto-shrink_400_400/0/1698643914901?e=1705536000&v=beta&t=lZQYDziB2Hk-Lb72S9Ls5ce0gmJVrrr2Jm3Oo8deoBY">
+                    <div class="team-content">
+                        <h3 class="title">NIKITAA NAIR</h3>
+                        <span class="post">Web Developer</span>
+                        <ul class="social">
+
+                            <li>
+                                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/nikitaanair" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-github"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/nikitaa-nair-684382227" target="_blank" rel="noopener noreferrer">
+                                    <i class="bi bi-linkedin"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS >
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+
+    <script>
+        $(function () {
+            $(document).scroll(function () {
+                var $nav = $("#mainNavbar");
+                $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+            });
+        });
+    </script>
+
+</body>
+
+</html-->
+<!--link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous"-->
+<!--link rel="stylesheet" href="team.css">
+<body>
+<div id="container">
+<div id="heading">
+<h1>OUR TEAM</h1>
+</div>
+<div class="item">
+<div class="front"> <img id="team1" class="team" src="https://www.supercuts.com/content/dam/supercuts/www/models/2017-models/1x1/heroes/supercuts-spencer-hero-hairstyles-classic-fade-1x1.png/_jcr_content/renditions/cq5dam.web.320.320.medium.png"> </div>
+<div class="back">
+<!p class="title">Tom Crow</p>
+<p class="job">Interface Designer</p> <a href="#"><i class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i class="fab fa-twitter-square social fa-2x"></i></a>
+</div>
+</div>
+<div class="item">
+<div class="front"> <img id="team2" class="team" src="https://www.eikonphoto.com/wp-content/uploads/2017/03/male-headshot-tie-color-e1515795186596.jpg"> </div>
+<div class="back">
+<p class="title">Tom Crow</p>
+<p class="job">Interface Designer</p> <a href="#"><i class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i class="fab fa-twitter-square social fa-2x"></i></a>
+</div>
+</div>
+<div class="item">
+<div class="front"> <img id="team3" class="team" src="https://www.ellasophiephoto.com/IMAGES/portraits/Portrait-Photographer-EllaSophie-OaklandStudio.jpg"> </div>
+<div class="back">
+<p class="title">Tom Crow</p>
+<p class="job">Interface Designer</p> <a href="#"><i class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i class="fab fa-twitter-square social fa-2x"></i></a>
+</div>
+</div>
+<div class="item">
+<div class="front"> <img id="team4" class="team" src="https://static1.squarespace.com/static/51047986e4b0f96ff4ffc8e0/t/58efb3ba5016e1685c551ae7/1492104152438/Mens-Accessories-Tie-Bow-pocket-square"> </div>
+<div class="back">
+<p class="title">Tom Crow</p>
+<p class="job">Interface Designer</p> <a href="#"><i class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i class="fab fa-twitter-square social fa-2x"></i></a>
+</div>
+</div>
+<div class="item">
+<div class="front"> <img id="team5" class="team" src="https://i.pinimg.com/originals/98/c8/0d/98c80de7cee9782322fdca55409bf6bc.jpg"> </div>
+<div class="back">
+<p class="title">Tom Crow</p>
+<p class="job">Interface Designer</p> <a href="#"><i class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i class="fab fa-twitter-square social fa-2x"></i></a>
+</div>
+</div>
+<div class="item">
+<div class="front"> <img id="team6" class="team" src="https://i.pinimg.com/originals/85/1e/7a/851e7a776927d4302e637ae511a3e372.jpg"> </div>
+<div class="back">
+<p class="title">Tom Crow</p>
+<p class="job">Interface Designer</p> <a href="#"><i class="fab fa-facebook-square social fa-2x"></i></a> <a href="#"><i class="fab fa-linkedin social fa-2x"></i></a> <a href="#"><i class="fab fa-twitter-square social fa-2x"></i></a>
+</div>
+</div>
+</div>
+</body-->
+
+<!DOCTYPE html>
+<html lang="en">
+  
+  
+<!--  Design by foolishdeveloper.com -->
+  
+  
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <title>Team Section </title>
+  <style media="screen">
+
+   * {
+     margin: 0;
+     padding: 0;
+     box-sizing: border-box;
+     font-family: Exo;
+   }
+   @font-face {
+     font-family: Exo;
+     src: url(./fonts/Exo2.0-Medium.otf);
+   }
+   .main{
+     width: 100%;
+     height: 100vh;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     background-color: #1e1f1f;
+   }
+   .profile-card{
+     position: relative;
+     font-family: sans-serif;
+     width: 220px;
+     height: 220px;
+     background: #fff;
+     padding: 30px;
+     border-radius: 50%;
+     box-shadow: 0 0 22px #3336;
+     transition: .6s;
+     margin: 0 25px;
+   }
+   .profile-card:hover{
+     border-radius: 10px;
+     height: 260px;
+   }
+   .profile-card .img{
+     position: relative;
+     width: 100%;
+     height: 100%;
+     transition: .6s;
+     z-index: 99;
+   }
+   .profile-card:hover .img{
+     transform: translateY(-60px);
+   }
+   .img img{
+     width: 100%;
+     border-radius: 50%;
+     box-shadow: 0 0 22px #3336;
+     transition: .6s;
+   }
+   .profile-card:hover img{
+     border-radius: 10px;
+   }
+   .caption{
+     text-align: center;
+     transform: translateY(-80px);
+     opacity: 0;
+     transition: .6s;
+   }
+   .profile-card:hover .caption{
+     opacity: 1;
+   }
+   .caption h3{
+     font-size: 21px;
+     font-family: sans-serif;
+     color: #0c52a1;
+   }
+   .caption p{
+     font-size: 15px;
+     color: #0c52a1;
+     font-family: sans-serif;
+     margin: 2px 0 9px 0;
+   }
+   .caption .social-links a{
+     color: #333;
+     margin-right: 15px;
+     font-size: 21px;
+     transition: .6s;
+   }
+   .social-links a:hover{
+     color: #0c52a1;
+   }
+
+  </style>
+
+</head>
+
+<body>
+    <div class="main">
+        <div class="profile-card">
+            <div class="img">
+                <img src="https://1.bp.blogspot.com/-8c7QTLoyajs/YLjr2V6KYRI/AAAAAAAACO8/ViVPQpLWVM0jGh3RZhh-Ha1-1r3Oj62wQCNcBGAsYHQ/s16000/team-1-3.jpg">
+            </div>
+            <div class="caption">
+                <h3>SAVIO JUBLET</h3>
+                <p>Senior App Developer</p>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </div>
+            </div>
+        </div>
+
+
+		<div class="profile-card">
+            <div class="img">
+                <img src="https://1.bp.blogspot.com/-8c7QTLoyajs/YLjr2V6KYRI/AAAAAAAACO8/ViVPQpLWVM0jGh3RZhh-Ha1-1r3Oj62wQCNcBGAsYHQ/s16000/team-1-3.jpg">
+            </div>
+            <div class="caption">
+                <h3>RENJITH</h3>
+                <p>Senior App Developer</p>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="profile-card">
+            <div class="img">
+                <img src="https://1.bp.blogspot.com/-vhmWFWO2r8U/YLjr2A57toI/AAAAAAAACO4/0GBonlEZPmAiQW4uvkCTm5LvlJVd_-l_wCNcBGAsYHQ/s16000/team-1-2.jpg">
+            </div>
+            <div class="caption">
+                <h3>SALMAN</h3>
+                <p>Front End Developer</p>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="profile-card">
+            <div class="img">
+                <img src="https://1.bp.blogspot.com/-AO5j2Y9lzME/YLjr3mxiqAI/AAAAAAAACPE/KAaYYTtQTrgBE3diTbxGoc4U4fCGx-C2gCNcBGAsYHQ/s16000/team-1-4.jpg">
+            </div>
+            <div class="caption">
+                <h3>NIKHILA</h3>
+                <p>Full Stact Developer</p>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
+<?php include_once "./include/footer.php";
